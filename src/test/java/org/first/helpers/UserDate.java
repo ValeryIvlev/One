dad17673b403ba59f990203322b0621e7c1978e6
Valery <qaivlev@yandex.ru>
2023-11-08 18:10:57 +0300

    firstCommit

--- src/test/java/org/first/helpers/UserDate.java
+++ src/test/java/org/first/helpers/UserDate.java
@@ -8,9 +8,6 @@ import java.util.Random;
 public class UserDate {
     private final Faker faker = new Faker(new Locale("ru"));
     private final Faker fakerEU = new Faker(new Locale("eu-US"));
-    private final Random random = new Random();
-    private final String [] genderRole = {"Male", "Female", "Other"};
-    private final String [] month = {"January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"};
     public String [] subjects = {"Arts", "Maths", "Hindi"};
     public String [] hobbies = {"Sports", "Reading", "Music"};
     public String lastName = faker.name().lastName();
@@ -19,8 +16,9 @@ public class UserDate {
     public String phoneNumber = faker.phoneNumber().subscriberNumber(10);
     public String currentAddress = faker.address().fullAddress();
     public String yearBirth = String.valueOf(faker.number().numberBetween(1960,1999));
-    public String gender = genderRole[random.nextInt(0, genderRole.length)];
-    public String mouthBirth = month[random.nextInt(0, month.length)];
+    public String gender =  faker.options().option("Male", "Female", "Other");
+    public String mouthBirth = faker.options().option("January", "February", "March", "April", "May", "June",
+            "July", "August", "September", "October", "November", "December");
     public String dayBirth = String.format("%02d", faker.number().numberBetween(1,28));
     public String state = "NCR";
     public String city = "Delhi";
